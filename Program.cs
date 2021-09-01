3e7986efc2820bff1c9e78b8021dd79159ed1012
Artem Gorbatuk <gorbatuk.am@gmail.com>
2021-09-01 14:20:12 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProgressInfo
+{
+    class Program
+    {
+        public static void DoSomething(IProgress<int> progress, CancellationToken token)
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(100); // < logic
+                progress?.Report(i);
+                token.ThrowIfCancellationRequested();
+            }
+        }
+
+        public static async Task Main(string[] args)
+        {
+            var progress = new Progress<int>(percent =>
+            {
+                Console.WriteLine(percent.ToString()); // < progress bar
+            });
+
+            var token = new CancellationToken();
+
+            await Task.Run(() => DoSomething(progress, token), token);
+
+            Console.ReadLine();
+        }
+    }
+}
